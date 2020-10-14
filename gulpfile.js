aedd9e4216fa6aa3c7ce18f0772833d321fa2ea1
Hung-Wei Chiu <hungwei@opennetworking.org>
2020-10-14 16:49:14 -0700

    Update 2020-10-14T23:49:13.176Z

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,19 @@
 const gulp = require('gulp');
 const gulpLoadPlugins = require('gulp-load-plugins');
+const sitemap = require('gulp-sitemap');
 
 const $ = gulpLoadPlugins();
 
 gulp.task('publish', () => {
   console.log('Publish Gitbook (_book) to Github Pages');
+  gulp.src('./_book/*/**/*.html', {
+            read: false
+        })
+        .pipe(sitemap({
+            siteUrl: 'http://ithome.hwchiu.com'
+        }))
+        .pipe(gulp.dest('./_book/'));
+
   return gulp.src('./_book/**/*')
     .pipe($.ghPages({
       origin: 'origin',
